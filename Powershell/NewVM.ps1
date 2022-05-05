0b7bdea56cbb4d986cdc2aa49018be9d13264741
Jared Shockley <jared@jshoq.com>
2022-05-05 09:41:12 -0700

    Initial code

---
+++ Powershell/NewVM.ps1
@@ -0,0 +1,15 @@
+New-AzVm `
+>>     -ResourceGroupName 'MTT-Demo-Posh' `
+>>     -Name 'PoshVM' `
+>>     -Location 'West US 3' `
+>>     -VirtualNetworkName 'myVnet' `
+>>     -SubnetName 'mySubnet' `
+>>     -SecurityGroupName 'myNetworkSecurityGroup' `
+>>     -PublicIpAddressName 'myPublicIpAddress' `
+>>     -OpenPorts 80,3389
+
+Remove-AzVm -ResourceGroupName 'MTT-Demo-Posh' -Name 'PoshVM' -Force
+
+Remove-AzResourceGroup -Name 'MTT-Demo-Posh' -Force
+
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
